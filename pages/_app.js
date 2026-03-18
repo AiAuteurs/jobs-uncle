153d28f92a008b94c80b29951d83ef6d49440670
AiAuteurs <153168008+AiAuteurs@users.noreply.github.com>
2026-03-18 11:11:12 +0700

    git commit -m "feat: add Vercel Analytics"

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import Script from 'next/script'
+import { Analytics } from '@vercel/analytics/react'
 
 export default function App({ Component, pageProps }) {
   return (
@@ -10,6 +11,7 @@ export default function App({ Component, pageProps }) {
         strategy="afterInteractive"
       />
       <Component {...pageProps} />
+      <Analytics />
     </>
   )
 }
