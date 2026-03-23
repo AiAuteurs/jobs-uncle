a3809a1ff4357b4aba0f42ac3c62d2a45ebf84e2
AiAuteurs <153168008+AiAuteurs@users.noreply.github.com>
2026-03-23 15:12:22 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import '../styles/globals.css'
 import Script from 'next/script'
+import Head from 'next/head'
 import { Analytics } from '@vercel/analytics/react'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Script
         src="https://cdn.promotekit.com/promotekit.js"
         data-promotekit="dec051b1-31b9-48f9-b6e9-dec5a86c96f9"
